c64fff4efe63b367ae18c60c75e52d98856946fd
til <m15071452418@163.com>
2025-05-30 19:31:25 +0800

    init

---
+++ src/main/java/com/til/light_iot_cloud/LightIotCloudApplication.java
@@ -0,0 +1,13 @@
+package com.til.light_iot_cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightIotCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LightIotCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/til/light_iot_cloud/lightiotcloud/LightIotCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.til.light_iot_cloud.lightiotcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightIotCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
